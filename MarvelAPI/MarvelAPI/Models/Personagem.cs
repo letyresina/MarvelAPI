686469337af180a1816ff002db253a14f3bc653e
letyresina <leticiaresina@gmail.com>
2022-09-12 20:50:23 -0300

    Criação da Model Personagem

---
+++ MarvelAPI/MarvelAPI/Models/Personagem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MarvelAPI.Models
+{
+    public class Personagem
+    {
+        public int IdCharacter { get; set; }
+        public string Name { get; set; }
+
+        public string NameStartsWith { get; set; }
+
+        public string Thumbnail { get; set; }
+
+        public int IdComics { get; set; }
+
+        public int IdSeries { get; set; }
+
+
+
+    }
+}

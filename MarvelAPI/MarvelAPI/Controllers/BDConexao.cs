5f0214fa577c1967407bafefe882dac63a124eee
letyresina <leticiaresina@gmail.com>
2022-09-12 21:48:02 -0300

    Feito a classe de conexão ao banco (não finalizada)

---
+++ MarvelAPI/MarvelAPI/Controllers/BDConexao.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MarvelAPI.Models;
+using System.Web;
+
+namespace MarvelAPI.Controllers
+{
+    public class BDConexao
+    {
+        MySqlConnection conexao;
+
+        //servidor qual está localizado o banco de dados
+        static string host = "localhost";
+
+        //nome do arquivo de banco de dados
+        static string database = "marvelapi.sql";
+
+        //usuário de conexão do banco
+        static string userDB = "root";
+
+        //senha do usuário para acesso
+        static string password = "leleca0404";
+
+        //string de conexão ao banco
+        public static string strProvider = "server=" + host +
+                                            ";Database=" + database +
+                                            ";User ID=" + userDB +
+                                            ";Password=" + password;
+
+        public static Boolean novo = false;
+        public String sql;
+
+
+
+
+    }
+}
--- MarvelAPI/MarvelAPI/Models/Quadrinhos.cs
+++ MarvelAPI/MarvelAPI/Models/Quadrinhos.cs
@@ -17,7 +17,5 @@ namespace MarvelAPI.Models
 
         public string Thumbnail { get; set; }
 
-
-
     }
 }

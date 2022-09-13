6b74c2d83db08c0d9c6fef32d130218949678d20
letyresina <leticiaresina@gmail.com>
2022-09-12 21:09:19 -0300

    Criação das Models das tabelas intermediárias

---
+++ MarvelAPI/MarvelAPI/Models/Personagem_Quadrinho.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MarvelAPI.Models
+{
+    public class Personagem_Quadrinho
+    {
+        public int IdCharacter { get; set; }
+
+        public int IdComics { get; set; }
+    }
+}

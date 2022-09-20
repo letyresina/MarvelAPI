7bd930c2f9c956a7d1db9f3e5ce79cdcaeb4fdc2
letyresina <leticiaresina@gmail.com>
2022-09-20 20:52:15 -0300

    Ajustes na view personagem

--- MarvelAPI/MarvelAPI/Controllers/PersonagemController.cs
+++ MarvelAPI/MarvelAPI/Controllers/PersonagemController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -24,22 +25,17 @@ namespace MarvelAPI.Controllers
         }
 
         // GET: api/Livro/getLivro/5
-        [HttpGet]
-        [ActionName("getPersonagem")]
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.ActionName("getPersonagem")]
         public Personagem Get(int id)
         {
             var personagem = personagens.FirstOrDefault((p) => p.IdCharacter == id);
-            if (personagem == null)
-            {
-                var resp = new HttpResponseMessage(HttpStatusCode.NotFound);
-                return resp;
-            }
             return personagem;
         }
 
         //exemplo de método com busca em Banco de dados
-        [HttpGet]
-        [ActionName("getAll")]
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.ActionName("getAll")]
         public IEnumerable GetAllLivros()
         {
             try
@@ -69,40 +65,5 @@ namespace MarvelAPI.Controllers
             return response;
         }
 
-        // PUT: api/Livro/5
-        [HttpPut]
-        [ActionName("updateItem")]
-        public HttpResponseMessage Put(int id, [FromBody] Livro item)
-        {
-
-            if (item == null)
-            {
-                return new HttpResponseMessage(HttpStatusCode.NotModified);
-            }
-
-            int index = livros.IndexOf((Livro)livros.Where((p) => p.Id == id).FirstOrDefault());
-            livros[index] = item;
-
-            return new HttpResponseMessage(HttpStatusCode.Accepted);
-        }
-
-        [HttpGet]
-        [ActionName("getByCategoria")]
-        public IEnumerable GetLivrosByCategory(string categoria)
-        {
-            return livros.Where(
-                (p) => string.Equals(p.Categoria, categoria,
-                    StringComparison.OrdinalIgnoreCase));
-        }
-
-        [HttpDelete]
-        [ActionName("delete")]
-        public HttpResponseMessage Delete(int id)
-        {
-            Livro l = (Livro)livros.Where((p) => p.Id == id);
-            int index = livros.IndexOf(l);
-            livros.RemoveAt(index);
-            return new HttpResponseMessage(HttpStatusCode.OK);
-        }
     }
 }
--- MarvelAPI/MarvelAPI/Models/Personagem.cs
+++ MarvelAPI/MarvelAPI/Models/Personagem.cs
@@ -7,6 +7,17 @@ namespace MarvelAPI.Models
 {
     public class Personagem
     {
+        private int v1;
+        private string v2;
+        private string v3;
+
+        public Personagem(int id, string nome, string thumbnail)
+        {
+            IdCharacter = id;
+            Nome = Nome;
+            Thumbnail = thumbnail;
+        }
+
         public int IdCharacter { get; set; }
 
         public string Nome { get; set; }
